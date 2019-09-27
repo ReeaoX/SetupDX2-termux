a4924a1eb57755fc982212916c37435cc7f52d11
KaliPi <pi@kali>
2019-09-27 00:34:13 -0700

    added lil code that displays if dx2 is installed or not on main menu

--- MainMenu.sh
+++ MainMenu.sh
@@ -9,11 +9,16 @@ function __drawtitlebar(){
 	clear
 	printf '\033[00;01;41;38;5;15m'
 	printf ' %.s' $(seq 1 $COLUMNS)
-	printf "\r $Title\033[m\n\n"
+	printf "\r $Title\033[m\n"
 }
 
 function __dx2_mainmenu(){
 	__drawtitlebar
+	if [ $DX2 ] && [ -d "$HOME"/.DX2 ] && [ -f "$HOME"/.dx2rc ]; then
+		echo -en "\033[00;03mDX2 Setup appears to be \033[32mcorrectly\033[00;03m installed on this system.\033[m\n"
+	else
+		echo -en "\033[00;03mDX2 Setup \033[31mdoesn't\033[00;03m appear to be correctly installed on this system.\033[m\n"
+	fi
 	echo -en """
 	[1]	Check
 	[2]	Setup

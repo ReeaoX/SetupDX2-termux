474682d6ee05736ec7f8b54ae37ce949104169ae
KaliPi <pi@kali>
2019-09-27 15:58:55 -0700

    updates

--- MainMenu.sh
+++ MainMenu.sh
@@ -14,17 +14,22 @@ function __drawtitlebar(){
 
 function __dx2_mainmenu(){
 	__drawtitlebar
-	if [ $DX2 ] && [ -d "$HOME"/.DX2 ] && [ -f "$HOME"/.dx2rc ]; then
-		echo -en "\033[00;03mDX2 Setup appears to be \033[32mcorrectly\033[00;03m installed on this system.\033[m\n"
+	if [ $DX2VERSION ]; then
+		echo -en "\033[00;03mDX2 Setup v"$DX2VERSION" was found \033[01;32minstalled\033[00;03m on this system.\033[m\n"
 	else
-		echo -en "\033[00;03mDX2 Setup \033[31mdoesn't\033[00;03m appear to be correctly installed on this system.\033[m\n"
+		echo -en "\033[00;03mDX2 Setup is \033[01;31mnot installed\033[00;03m on this system.\033[m\n"
 	fi
 	echo -en """
-	[1]	Check
-	[2]	Setup
-	[3]	Addons
+	\033[00;;01;38;5;46m[\033[00;01m1\033[00;;01;38;5;46m]\033[00;01m	(Re)Install / Update DX2Setup
+	\033[00;;01;38;5;46m[\033[00;01m2\033[00;;01;38;5;46m]\033[00;01m	Install Addons
+	\033[00;;01;38;5;46m[\033[00;01m3\033[00;;01;38;5;46m]\033[00;01m	Check for updates
 
-: """
+	\033[00;;01;38;5;190m[\033[00;01mH\033[00;;01;38;5;190m]\033[00;01m	Help
+	\033[00;;01;38;5;196m[\033[00;01mX\033[00;;01;38;5;196m]\033[00;01m	Exit
+
+ : """
+ 	read -n 1 -r MAINMENUOPT
+
 }
 
 __dx2_mainmenu

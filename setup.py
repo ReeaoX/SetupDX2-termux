408625450499cf9a97f1fc97eaf392e7367a7d8e
ReeaoX <reeaox@gmail.com>
2024-12-09 21:49:36 +0800

    Rewrite 'setup.py', but switch into Termux support.

--- setup.py
+++ setup.py
@@ -1,104 +1,110 @@
-#!/usr/bin/env python3
+#!/data/data/com.termux/files/usr/bin/python3
+ImportError_message = "To setup DX2 runtime, Python modules is required."
 
-import os
-import re
-import shutil
+try:
+  import os
+  from decouple import config as conf
+  from rich.prompt import Confirm as ryn
+  from dotenv import find_dotenv as dotfind, load_dotenv as dotload
+  from pathlib import Path as pydir, PurePath as puredir
+  from click import clear as cls
+  import re
+  from shutil import copy2 as pycopy
+except ImportError:
+  raise ImportError(ImportError_message)
 
-__VERSION__ = '4.2'
-COLS, LINES = shutil.get_terminal_size()
-HOME = os.getenv('HOME')
+"""Load '.env'-formatted config (with 'dotenv')."""
+dotenv_file = ("dx%s" % "2" + "-" + "config" + ".%s" % "env") # identified as "dx2-config.env".
+dotload(dotfind(dotenv_file))
+
+__VERSION__ = conf("DX2_PYTHON_SCRIPT_VERSION")
+COLUMN = conf("COLUMNS")
+LINES = conf("LINES")
+PREFIX = conf("PREFIX", default="/")
+SHELL = conf("SHELL")
+PREFIX_ETC = puredir(PREFIX, "etc")
+PREFIX_BIN = puredir(PREFIX, "bin")
+HOME = conf("HOME", default="/root")
 installyn = None
 
-## DX2 VARS
-DX2 = (HOME + '/.DX2')
-DX2BIN = (DX2 + '/bin')
-DX2RC = (DX2 + '/rc')
-DX2DATA = (DX2 + '/data')
-DX2FUNCS = (DX2 + '/functions')
-DX2BAK = (DX2 + '/backups')
-DX2TMP = (DX2 + '/temp')
-DX2GIT = (DX2 + '/git')
-DX2AKA = (DX2 + '/aliases')
-DX2SHORTCUTS = (DX2 + '/.Shortcuts')
+"""DX2 variables(?)."""
+DX2 = conf("DX2")
+DX2BIN = conf("DX2BIN")
+DX2RC = conf("DX2RC")
+DX2DATA = conf("DX2DATA")
+DX2RC_RUNTIME = conf("DX2RC_RUNTIME")
+DX2FUNCS = conf("DX2FUNCS")
+DX2BAK = conf("DX2BAK")
+DX2ALIASES = conf("DX2ALIASES", default="{0}/.dx2-aliases".format(HOME))
+DX2TMP = conf("DX2TMP")
+DX2GIT = conf("DX2GIT")
+modifiedcd_function_name = "cd"
+DX2_CDPATH = conf("DX2_CDPATH")
+DX2AKA = conf("DX2AKA")
+DX2SHORTCUTS = conf("DX2SHORTCUTS")
 
-print('')
+cls()
 
+"""The 'files' recursive class."""
 class files:
-	loaddx2rc = ("""
-### BEG - LOAD DX2RC ###
-if [ -f ~/.dx2rc ]; then
-	. ~/.dx2rc;
-fi
-### END - LOAD DX2RC ###
-""")
-	dx2rc = ("""## Load VARs
-#
-export DX2="$HOME/.DX2"
-export DX2VERSION=""" + '"' + str(__VERSION__) + '"' + """
-export DX2BIN="$DX2/bin"
-export DX2DATA="$DX2/data"
-export DX2RC="$DX2/rc"
-export DX2BAK="$DX2/backups"
-export DX2TMP="$DX2/temp"
-export DX2GIT="$DX2/git"
-export DX2FUNCS="$DX2/functions"
-export DX2ALIASES="$DX2/aliases"
-export DX2SHORTCUTS="$DX2/.Shortcuts"
-
-
-## Load everything in $DX2FUNCS
-#
-if [ "$(ls $DX2FUNCS | wc -l)" != 0 ]; then
+	loaddx2rc = ("""if [[ -f {0} ]]; then
+	source {0};
+fi""".format(DX2RC_RUNTIME))
+	dx2rc = ("""export DX2="{DX2}"
+export DX2VERSION=""" + '"%s"' % str(__VERSION__) + """
+export DX2BIN="{0}"
+export DX2DATA="{1}"
+export DX2RC="{2}"
+export DX2BAK="{3}"
+export DX2TMP="{4}"
+export DX2GIT="{5}"
+export DX2FUNCS="{6}"
+export DX2ALIASES="{7}"
+export DX2SHORTCUTS="{8}"
+""".format(DX2BIN, DX2BIN, DX2DATA, DX2RC, DX2BAK, DX2TMP, DX2GIT, DX2ALIASES, DX2SHORTCUTS) + """
+if [[ -n "$(ls $DX2FUNCS)" ]]; then
 	for FUNCSFILES in $DX2FUNCS/*; do
-		if [ -r "$FUNCSFILES" ]; then
+		if [[ -r "$FUNCSFILES" ]]; then
 			source "$FUNCSFILES";
 		fi
 	done
 fi
-
-## Load everything in $DX2RC
-#
-if [ "$(ls $DX2RC | wc -l)" != 0 ]; then
+""" + """
+if [[ -n "$(ls $DX2RC)" ]]; then
 	for RCFILES in $DX2RC/*; do
-		if [ -r "$RCFILES" ]; then
+		if [[ -r "$RCFILES" ]]; then
 			source "$RCFILES";
 		fi
 	done
 fi
-
-
-## Load everything in $DX2ALIASES
-#
-if [ "$(ls $DX2ALIASES | wc -l)" != 0 ]; then
+""" + """
+if [[ -n "$(ls $DX2ALIASES)" ]]; then
 	for ALIASESFILES in $DX2ALIASES/*; do
 		if [ -r "$ALIASESFILES" ]; then
 			source "$ALIASESFILES";
 		fi
 	done
 fi
-
+""" + """
 if [ "$(echo $PATH | grep -qe '.DX2/bin'; echo $?)" != '0' ]; then
-	export PATH="$DX2BIN:$PATH";
+	export PATH+=":$DX2BIN";
 fi
-
-export CDPATH=~/.Shortcuts
-""")
+""" + """
+export CDPATH=%s""" % DX2_CDPATH)
 
 class stringsforfiles:
-	modifiedcd = ("""cd(){
-	builtin cd $*
+	modifiedcd = ("""function %s () {
+	builtin %s $*
 	LC_COLLATE='C' \ls -ACw $COLUMNS --color=always --group-directories-first
-}""")
+}""" % modifiedcd_function_name)
 	defaultls = ("""export LS_OPTS="-C -w \$COLUMNS --color=always --group-directories-first"
 export LC_COLLATE='C'
-
+""" + """
 alias ls="\ls $LS_OPTS"
 alias l=ls
 alias la='ls -A'
 alias ll='ls -l'
-alias lla='ls -Al'
-""")
-
+alias lla='ls -Al'""")
 
 class tools():
 	def titlebar(x, y, z):
@@ -115,80 +121,79 @@ class tools():
 			FG = ('\033[38;5;' + str(z) + 'm')
 		print('\033c' + BG + FG + ' ' * COLS + '\r ' + str(TITLE) + '\033[m\n')
 		del BG, FG, TITLE
+
 	def subtitle(x):
 		subtitle = str(x)
 		print('\n\033[00;01;38;5;196m[\033[00;01m-\033[00;01;38;5;196m]\033[00;01m - ' + str(subtitle) + '\033[m')
 		del subtitle
 
-
-
 class funcs:
 	def checkdirs(x):
 		dirs = str(x)
 		shortdir = re.sub(HOME, '~', dirs)
-		print(' \033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m Creating \033[38;5;51m' + str(shortdir), end='')
+		print('\033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m Creating \033[38;5;51m' + str(shortdir), end='')
+
 		if os.path.exists(dirs) == True:
-			print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
-#			print('\r \033[00;01;38;5;51m[\033[00;01mi\033[00;01;38;5;51m] \033[m')
-#			print('\t\033[00;01;32mDirectory already exists.')
+                        print('\r \033[00;01;38;5;46m[\033[00;01m✓\033[00;01;38;5;46m] \033[m')
+                        # print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
+                        # print('\r \033[00;01;38;5;51m[\033[00;01mi\033[00;01;38;5;51m] \033[m')
+                        # print('\t\033[00;01;32mDirectory already exists.')
 		else:
 			os.mkdir(dirs)
-			print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
+                        print('\r \033[00;01;38;5;46m[\033[00;01m✓\033[00;01;38;5;46m] \033[m')
+			# print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
 		del dirs
 
 	def detectdx():
 		init = int('0')
 		print("Checking system for any previous versions of DX2 Setup: ")
-		print(' \033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m\tChecking for DX2 vars.', end='')
+		print('\n\033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m\tChecking for DX2 vars.', end='')
+
 		if os.getenv('DX2VERSION') is not None:
 			print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
 			init = int(init) + int('1')
 		else:
 			print('\r \033[00;01;38;5;196m[\033[00;01m✖\033[00;01;38;5;196m] \033[m')
 			init = int(init) + int('0')
-		print(' \033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m\tChecking for DX2 directories.', end='')
+
+		print('\033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m\tChecking for DX2 directories.', end='')
 		if os.path.exists(HOME + '.DX2') == True:
 			print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
 			init = int(init) + int('1')
 		else:
 			print('\r \033[00;01;38;5;196m[\033[00;01m✖\033[00;01;38;5;196m] \033[m')
-#			installyn = '1'
+                        # installyn = '1'
 			init = int(init) + int('0')
-		print(' \033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m\tChecking for ~/.dx2rc.', end='')
+
+		print('\033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m\tChecking for ~/.dx2rc.', end='')
 		if os.path.exists(HOME + '/.dx2rc') == True:
 			print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
 			init = int(init) + int('1')
 		else:
 			print('\r \033[00;01;38;5;196m[\033[00;01m✖\033[00;01;38;5;196m] \033[m')
-#			installyn = '1'
+                        # installyn = '1'
 			init = int(init) + int('0')
+
 		if init == int('0'):
-			print('\nDX2 was not found')
+			print("\nDX2 was not found", end="\r\n")
 			funcs.asktoinstall()
 		elif init == int('3'):
-			print('\nDX2 detected / installed')
+			print("\nDX2 detected / installed", end="\r\n")
 		else:
-			print('\nDX2 found, but some elements are missing')
+			print("\nDX2 found, but some elements are missing", end="\r\n")
 			funcs.asktoinstall()
 
 	def asktoinstall():
-		ans = input("""
-Do you want to install DX2 now?
-[Y/n]: """)
-		if ans == 'y':
-			print('installing...')
-		elif ans == 'Y':
-			print('installing...')
-		elif ans == '':
-			print('installing...')
-		elif ans == 'n':
-			print('exiting...')
-		elif ans == 'N':
-			print('exiting...')
+		ans = ryn.ask("Do you need installing DX2?")
 
-class install:
+		if ans == "y":
+			print("Installing...")
+		elif ans == "n":
+			rsys.exit("Exiting...")
+
+class funcs_extra:
 	def dirs():
-		tools.subtitle('Setting up directories:')
+		tools.subtitle("Setting up directories:")
 		funcs.checkdirs(DX2)
 		funcs.checkdirs(DX2RC)
 		funcs.checkdirs(DX2BIN)
@@ -199,61 +204,96 @@ class install:
 		funcs.checkdirs(DX2GIT)
 		funcs.checkdirs(DX2SHORTCUTS)
 		funcs.checkdirs(DX2AKA)
+
 	def dx2rc():
-		tools.subtitle('Creating ~/.dx2rc')
-		print(' \033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m Creating \033[38;5;51m~/.dx2rc', end='')
-		f = open(HOME + '/.dx2rc', "w")
-		f.write(files.dx2rc)
-		f.close()
+		tools.subtitle("Creating ~/.dx2rc")
+		print('\033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m Creating \033[38;5;51m~/.dx2rc', end='')
+                f_dir = puredir(HOME, ".dx2rc")
+
+                if not str(f_dir) == ("%s/.dx2rc" % HOME):
+                   f_dir = puredir(HOME, "/", ".dx2src")
+
+		with open(str(f_dir), "w") as f:
+		     f.write(files.dx2rc)
+                     f.flush()
 		print('\r \033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m] \033[m')
-	def bakbashrc():
-		'''
-		If ~/.bashrc is found, copy the file into $DX2BAK
-		'''
-		SHELLRC = (HOME + '/.bashrc')
-		if os.path.exists(SHELLRC) == True:
-			shutil.copyfile(SHELLRC, DX2BAK + '/.bashrc')
+
+	def bak_shellrc():
+		"""If ~/.bashrc is found, copy the file into $DX2BAK"""
+                PREFIX_BIN_STR = str(PREFIX_BIN)
+                SHELL_FORMAT == SHELL.split(PREFIX_BIN_STR + "/")[1]
+                PREFIX_ETC_STR = str(PREFIX_ETC)
+
+                if SHELL_FORMAT == "bash":
+                   if pydir("%s/.bashrc" % HOME).exists() == True:
+		      SHELLRC = puredir(HOME, ".bashrc")
+                   elif pydir("%s/.bashrc" % HOME).exists() == False:
+                      SHELLRC = puredir(PREFIX_ETC_STR, "bash.bashrc")
+                elif SHELL_FORMAT == "xonsh":
+                   if pydir("%s/.config" % HOME + "/" + SHELL_FORMAT + "/" + "%s.xsh" % "rc").exists() == True:
+                      SHELLRC = puredir(HOME, ".config", SHELL_FORMAT, ("%s.xsh" % "rc"))
+                   elif pydir("%s/.config" % HOME + "/" + SHELL_FORMAT + "/" + "%s.xsh" % "rc").exists() == False:
+                      SHELLRC = puredir(HOME, ".xonshrc")
+                elif not SHELL_FORMAT == "xonsh":
+                   SHELLRC = puredir(HOME, (".%src" % SHELL_FORMAT))
+
+                SHELLRC_STR = str(SHELLRC)
+		if pydir(SHELLRC_STR).exists() == True:
+                   pycopy(SHELLRC, [str(DX2_SHELLRC) for DX2_SHELLRC in puredir(DX2BAK, (".%src" % SHELL_FORMAT))])
+
 	def editbashrc():
-		'''
-		Checks to see if .bashrc has been edited to load .dx2rc or
+		"""Checks to see if .bashrc has been edited to load .dx2rc or
 		not. If not, create a new file with the old .bashrc files
-		with the bit to load dx2rc at the end.
-		'''
-		SHELLRC = (HOME + '/.bashrc')
-		f = open(SHELLRC,"r")
-		bashrcfile = f.read()
-		f.close()
+		with the bit to load dx2rc at the end."""
+		PREFIX_BIN_STR = str(PREFIX_BIN)
+                SHELL_FORMAT == SHELL.split(PREFIX_BIN_STR + "/")[1]
+                PREFIX_ETC_STR = str(PREFIX_ETC)
+
+                if SHELL_FORMAT == "bash":
+                   if pydir("%s/.bashrc" % HOME).exists() == True:
+                      SHELLRC = puredir(HOME, ".bashrc")
+                   elif pydir("%s/.bashrc" % HOME).exists() == False:
+                      SHELLRC = puredir(PREFIX_ETC_STR, "bash.bashrc")
+                elif SHELL_FORMAT == "xonsh":
+                   if pydir("%s/.config" % HOME + "/" + SHELL_FORMAT + "/" + "%s.xsh" % "rc").exists() == True:
+                      SHELLRC = puredir(HOME, ".config", SHELL_FORMAT, ("%s.xsh" % "rc"))
+                   elif pydir("%s/.config" % HOME + "/" + SHELL_FORMAT + "/" + "%s.xsh" % "rc").exists() == False:
+                      SHELLRC = puredir(HOME, ".xonshrc")
+                elif not SHELL_FORMAT == "xonsh":
+                   SHELLRC = puredir(HOME, (".%src" % SHELL_FORMAT))
+
+                SHELLRC_STR = str(SHELLRC)
+		with open(SHELLRC, "r") as shell_content
+		     bashrcfile = shell_content.read()
+
 		if files.loaddx2rc not in bashrcfile:
-			f = open(SHELLRC, "w")
-			f.write(bashrcfile + files.loaddx2rc)
-			f.close()
-
-
+                        with open(SHELLRC, "w") as shell_content_2:
+			     shell_content_2.write(bashrcfile + files.loaddx2rc)
+			     shell_content_2.flush()
 
 	def editshellrc():
-		install.bakbashrc()
-		install.editbashrc()
-#		tools.subtitle('Editing Shells rc file')
-#		print(' \033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m Detecting Shell', end='')
-#		if os.getenv('BASH') is not None:
-#			SHELL = 'bash'
-#			SHELLRC = (HOME + '/.' + str(SHELL) + 'rc')
-#		elif os.getenv('ZSH') is not None:
-#			SHELL = 'zsh'
-#			SHELLRC = (HOME + '/.' + str(SHELL) + 'rc')
-#		else:
-#			print('Shell not found')
+		funcs_extra.bakbashrc()
+		funcs_extra.editbashrc()
+                # tools.subtitle('Editing Shells rc file')
+                # print(' \033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m Detecting Shell', end='')
+                # if os.getenv('BASH') is not None:
+                # SHELL = 'bash'
+                # SHELLRC = (HOME + '/.' + str(SHELL) + 'rc')
+                # elif os.getenv('ZSH') is not None:
+                #	SHELL = 'zsh'
+                #	SHELLRC = (HOME + '/.' + str(SHELL) + 'rc')
+                # else:
+                #	print('Shell not found')
 
 	def install():
 		tools.titlebar('DX2 Setup', 'bg', 'fg')
-		install.dirs()
-		install.dx2rc()
-		install.editshellrc()
-
+		funcs_extra.dirs()
+		funcs_extra.dx2rc()
+		funcs_extra.editshellrc()
 
 #\033[00;01;38;5;190m[\033[00;01m⌛\033[00;01;38;5;190m]\033[m
 #\r\033[00;01;38;5;46m[\033[00;01m✔\033[00;01;38;5;46m]\033[m
 #\r\033[00;01;38;5;196m[\033[00;01m✖\033[00;01;38;5;196m]\033[m
 
 #funcs.detectdx()
-install.install()
+funcs_extra.install()
